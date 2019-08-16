d447c11e85d8d02a8913e6b8527329a0b19d0df3
Marcos Aurélio Pereira Júnior <marcos.junior@zup.com.br>
2019-08-16 20:47:55 -0300

    Hello world

---
+++ src/utils/forecast.js
@@ -0,0 +1,34 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/63de60e7d8731896a3ec92b03859af17/" +
+    latitude +
+    "," +
+    longitude +
+    "?units=si";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degress out. This high today is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
